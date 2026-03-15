0b10335bd55a274c1e8f1587f7cabdad2175a48d
Riddhi <riddhcodes@gmail.com>
2026-03-15 15:14:27 +0530

    Sync vite.config.ts and .gitignore with Person A's versions

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      include: ["buffer", "crypto", "stream", "util"],
-      globals: {
-        Buffer: true,
-        global: true,
-        process: true,
+  plugins: [react()],
+  define: {
+    "process.env": {},
+    global: "globalThis",
+  },
+  resolve: {
+    alias: {
+      stream: "rollup-plugin-node-polyfills/polyfills/stream",
+      buffer: "rollup-plugin-node-polyfills/polyfills/buffer-es6",
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
       },
-    }),
-  ],
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()],
+    },
+  },
 });
