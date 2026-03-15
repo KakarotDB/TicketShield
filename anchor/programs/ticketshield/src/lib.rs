894b14f03f4d572ffd34995a0c2c9a16ba1db59f
KakarotDB <mrigajpersonal@gmail.com>
2026-03-15 08:21:47 +0000

    fix program ID back to our deployed address

--- anchor/programs/ticketshield/src/lib.rs
+++ anchor/programs/ticketshield/src/lib.rs
@@ -9,7 +9,7 @@ use instructions::*;
 // Replace with real program ID after running:
 //   anchor build
 //   solana-keygen pubkey target/deploy/ticketshield-keypair.json
-declare_id!("3CWBxYn3dm7ZTgSzu4WnhLM8CtD2SzAPoiHukhbnikT7");
+declare_id!("Ft9ag3LtV2PWM5vADsyj9sEfthoWYKgwLiX3XxZtKoPX");
 
 #[program]
 pub mod ticketshield {
